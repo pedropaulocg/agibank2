b881067501fead3046a28629d5c25ca70c255d42
Pedro Paulo <87837439+pedropaulocg@users.noreply.github.com>
2022-03-16 12:59:20 -0300

    Add files via upload

---
+++ assests/js/hover.js
@@ -0,0 +1,8 @@
+function hoverAll(button){
+    button.style.transition = '.3s'
+    button.style.transform = 'scale(1.1)'
+}
+function hoverOutAll(buttonOut){
+    buttonOut.style.transition = '.3s'
+    buttonOut.style.transform = 'scale(1)'
+}
